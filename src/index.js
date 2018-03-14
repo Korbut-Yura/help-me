061ec4a0f91d29363a7e578f6599d7665466ff62
Yura-Korbut <korbuty@mail.ru>
2018-03-14 23:49:53 +0300

    feature/solution 4/17

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function count(s, pairs) {
-  // your implementation
+  let N = 1,
+    K = 0;
+  for (let i=0; i < pairs.length; i++) {
+    N *= pairs[i][0];
+  }
+  for (let i=0; i < pairs.length; i++) {
+    K += N/pairs[i][0];
+    for (let j = 0; j < i; j++) {
+      K -= N/(pairs[i][0]*pairs[j][0]) 
+    }
+  }
+ 
+  for (let j =0; j < s.length; j++) {
+    if (s[j] == 1) {
+      K = N-K
+    }
+  }
+
+  const result = K % 1000000007;
+  return result;
 }
